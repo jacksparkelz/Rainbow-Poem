45cfc1a113c6d9540e8f76917c0bcba192c9c845
jacksparkelz <119563066+jacksparkelz@users.noreply.github.com>
2022-12-01 01:35:09 -0500

    This is the calculator project

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+package edu.frostburg.cosc240;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		accumulate(in);
+		add(in);
+	}
+	
+	/**
+	 * Adds a bunch of numbers together until user types -1
+	 * 
+	 * @param in Scammer for keyboard input.
+	 */
+	public static void accumulate(Scanner in) {
+		int sum = 0;
+		boolean go = true;
+
+		while(go) {
+			int userNum = in.nextInt();
+			if(userNum == -1) {
+				go = false;
+			}else if(userNum < 0){
+				System.err.println("Can't do that!");
+			}else {
+			}
+				sum = sum + userNum;
+			}
+		}
+		
+		
+		
+		System.out.println(sum);
+	}
+	
+	public static void add(Scanner in) {
+		
+	}
+
+}
